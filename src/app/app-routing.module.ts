b04ae52316453ee027f6798afd5f6bdb01b2ea3f
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-05 12:21:03 -0300

    routes listar-pensamento and criar-pensamento add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListarPensamentoComponent} from './componentes/pensamentos/listar-pensamento/listar-pensamento.component'
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'listarPensamento', pathMatch:'full'},
+  {path:'listarPensamento', component: ListarPensamentoComponent},
+  {path:'criarPensamento', component: CriarPensamentoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
