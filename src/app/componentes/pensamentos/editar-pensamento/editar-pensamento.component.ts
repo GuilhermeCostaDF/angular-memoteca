85e14dc4747ad475499cf8b2a183b1b78962daa6
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-06 13:12:03 -0300

    editar pensamento component

---
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -0,0 +1,26 @@
+import { Pensamento } from './../pensamento';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-pensamento',
+  templateUrl: './editar-pensamento.component.html',
+  styleUrls: ['./editar-pensamento.component.css']
+})
+export class EditarPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento={
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  editarPensamento(){}
+
+  cancelar(){}
+
+}
